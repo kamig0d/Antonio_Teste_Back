c18c6f866933e01fb3bf52dac1699915cf102b29
kamig0d <juninhobarbosa123@hotmail.com>
2018-05-02 22:30:42 -0300

    resolve merge conflict

--- src/valemobi_teste_back/DAO.java
+++ src/valemobi_teste_back/DAO.java
@@ -10,7 +10,6 @@ public class DAO {
     public void salvar(Cadastro c) throws SQLException{
         Connection conn = null;
         conn = Connector.GetDerbyConnection();
-<<<<<<< HEAD
         PreparedStatement ps = conn.prepareStatement("INSERT INTO TB_CUSTOMER_ACCOUNT (ID_CUSTOMER, CPF_CNPJ, NM_CUSTOMER, IS_ACTIVE, VL_TOTAL) VALUES (?,?,?,?,?)");
         int i = 0;
         ps.setInt(++i, c.getId_customer());
@@ -18,14 +17,7 @@ public class DAO {
         ps.setString(++i, c.getName());
         ps.setBoolean(++i, c.isActive());
         ps.setDouble(++i, c.getTotal());
-=======
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO TB_CUSTOMER_ACCOUNT (CPF_CNPJ, NM_CUSTOMER, IS_ACTIVE, VL_TOTAL) VALUES (?,?,?,?)");
-        
-        ps.setString(1, c.getCpf_cnpj());
-        ps.setString(2, c.getName());
-        ps.setBoolean(3, c.active);
-        ps.setDouble(4, c.getTotal());
->>>>>>> bf8254a3d0f119d07b540e19b79869333ad73cec
+       
         
         try{
             ps.execute();
